f6e96d919db06feb339a144fe5a86a9f01188825
Loic2888 <12188@holbertonstudents.com>
2026-08-03 20:37:17 +0200

    feat: update ESLint configuration to include global ignores and JSX runtime

--- front_end-frameworks/react/eslint.config.js
+++ front_end-frameworks/react/eslint.config.js
@@ -1,9 +1,11 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
+  globalIgnores(["dist"]),
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
 ]);
