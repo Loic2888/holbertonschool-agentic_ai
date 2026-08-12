bd5573de757d8c7a85643651a49abeffff9b8e01
Loic2888 <12188@holbertonstudents.com>
2026-08-12 14:28:16 +0200

    feat: initialize Svelte project with Vite, Tailwind CSS, and essential components
    - Added package.json for project dependencies and scripts
    - Created favicon.svg for the application
    - Developed main App.svelte component with a Counter component
    - Implemented Counter.svelte for interactive counting functionality
    - Added features.js to define application features with icons
    - Created insights.js to provide insights data for the application
    - Developed steps.js to outline the process steps for the application
    - Included global.css for Tailwind CSS integration
    - Set up main.js to mount the Svelte application
    - Created insightsService.js to simulate fetching insights data
    - Added vite-env.d.ts for TypeScript support in Vite
    - Configured svelte.config.js for Svelte project settings
    - Established vite.config.js for Vite configuration with server settings

---
+++ front_end-frameworks/svelte/src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Counter from './components/Counter.svelte'
+</script>
+
+<section id="center" class="min-h-screen bg-slate-950 flex flex-col items-center justify-center text-white">
+  <div>
+    <h1>Get started</h1>
+    <Counter />
+  </div>
+</section>
---
+++ front_end-frameworks/svelte/src/services/insightsService.js
@@ -0,0 +1,11 @@
+// src/services/insightsService.js
+import { insights } from '../data/insights';
+
+
+export const getInsights = async () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(insights);
+    }, 1000);
+  });
+};
